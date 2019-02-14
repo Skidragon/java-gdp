ea361bc940af58d3aff0871c7182b22acebe73e9
Simon Davis <simon_freelance@hotmail.com>
2019-02-14 16:54:16 -0500

    GET /names route finished, countries are alphabetized by name

--- src/main/java/com/lambdaschool/gdp/CountryController.java
+++ src/main/java/com/lambdaschool/gdp/CountryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -19,10 +20,18 @@ public class CountryController {
         this.countryRepo = countryRepo;
         this.rt = rt;
     }
-//    /names - return using the JSON format all of the countries alphabetized by name
     @GetMapping("/countries")
     public List<Country> all() {
         return countryRepo.findAll();
+    }
+    //    /names - return using the JSON format all of the countries alphabetized by name
+
+    @GetMapping("/names")
+    public List<Country> alphabetizeNames() {
+        return countryRepo.findAll()
+                .stream()
+                .sorted((c1,c2) -> c1.getCountry().compareToIgnoreCase(c2.getCountry()))
+                .collect(Collectors.toList());
     }
     @PostMapping("/countries")
     public List<Country> newCountries(@RequestBody List<Country> newCountries) {
